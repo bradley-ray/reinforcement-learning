65e634bf6a1b2f8a77a6c0cbaf1183983b6a50e1
Bradley Ray <bradleyr320@gmail.com>
2023-03-01 09:39:09 -0600

    added replay buffer

--- models/dqn.py
+++ models/dqn.py
@@ -1,22 +1,34 @@
 import torch
 import torch.nn as nn
 
+import numpy as np
+
 class ReplayBuffer:
-    ...
+    def __init__(self):
+        self.__buf = []
+
+    def add(self, transition):
+        self.__buf.append(transition)
+
+    def sample(self, size):
+        idxs = np.random.choice(len(self.__buf), size, replace=False)
+        return [self.__buf[i] for i in idxs]
 
 # as outlined in [Playing Atari with Deep Reinforcement Learning](https://arxiv.org/pdf/1312.5602.pdf)
 class DQN(nn.Module):
     def __init__(self, num_actions):
-        self.model = nn.Sequential([
-            nn.Conv2d(1, 16, 8, 4),
+        super().__init__()
+        self.model = nn.Sequential(*[
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(16, 32, 4, 2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Flatten(),
+            nn.Linear(2592, 256),
             nn.ReLU(),
             nn.Linear(256, num_actions),
         ])
 
     def forward(self, x):
-        out = model(x)
+        out = self.model(x)
         return out
