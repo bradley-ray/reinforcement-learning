4155d6b085cc8265b020d4929af26fedd566af1d
Bradley Ray <bradleyr320@gmail.com>
2023-03-14 19:58:40 -0500

    added pixels network

--- deep-q-network.py
+++ deep-q-network.py
@@ -4,13 +4,24 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as T
 
+import cv2
 from PIL import Image
 import numpy as np
 import gymnasium as gym
 
+import os
+
+DEVICE = os.getenv("DEVICE", "cpu")
+
+def transforms(env):
+    obs = cv2.resize(env.render(), (110,84))
+    obs = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+    obs = torch.from_numpy(obs).float().reshape((1, 84, 110)).to(DEVICE)
+    return obs
+
 def env_to_img(arr):
   to_img = T.ToPILImage()
-  arr = torch.from_numpy(arr).permute(2,0,1)
+  arr = torch.from_numpy(arr).permute(2,0,1).to(DEVICE)
   img = to_img(arr)
   return img
 
@@ -31,17 +42,32 @@ class DQNSimple(nn.Module):
         out = self.ff2(out)
         return out
 
+class DQNPixel(nn.Module):
+    def __init__(self, act_dim, h_dim):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, 8, 4)
+        self.conv2 = nn.Conv2d(16, 32, 4, 2)
+        self.ff1 = nn.Linear(3456, h_dim)
+        self.ff2 = nn.Linear(h_dim, act_dim)
+
+    def forward(self, obs):
+        out = self.conv1(obs).relu()
+        out = self.conv2(out).relu().flatten(1)
+        out = self.ff1(out).relu()
+        out = self.ff2(out)
+        return out
+
 class ReplayBuffer:
     def __init__(self, capacity, obs_dim, act_dim):
         self.capacity = capacity
         self.idx = 0
         self.size = 0
 
-        self.obs_buf = torch.zeros((self.capacity, *obs_dim), requires_grad=False).float()
-        self.act_buf = torch.zeros((self.capacity, 1), requires_grad=False).long()
-        self.rew_buf = torch.zeros((self.capacity, 1), requires_grad=False).float()
-        self.done_buf = torch.zeros((self.capacity, 1), requires_grad=False).float()
-        self.obs_n_buf = torch.zeros((self.capacity, *obs_dim), requires_grad=False).float()
+        self.obs_buf = torch.zeros((self.capacity, *obs_dim), requires_grad=False).float().to(DEVICE)
+        self.act_buf = torch.zeros((self.capacity, 1), requires_grad=False).long().to(DEVICE)
+        self.rew_buf = torch.zeros((self.capacity, 1), requires_grad=False).float().to(DEVICE)
+        self.done_buf = torch.zeros((self.capacity, 1), requires_grad=False).float().to(DEVICE)
+        self.obs_n_buf = torch.zeros((self.capacity, *obs_dim), requires_grad=False).float().to(DEVICE)
 
     def append(self, exp):
         self.obs_buf[self.idx] = exp[0]
@@ -68,13 +94,20 @@ class ReplayBuffer:
 
 class Agent:
     def __init__(self, env, model, *args):
+        self.pixels = model != DQNSimple
         self.env = env
-        self.memory = ReplayBuffer(10_000, env.observation_space.shape, int(env.action_space.n))
-        self.q = model(*args)
+        if self.pixels:
+            self.memory = ReplayBuffer(10_000, (1, 84, 110), int(env.action_space.n))
+        else:
+            self.memory = ReplayBuffer(10_000, env.observation_space.shape, int(env.action_space.n))
+        self.q = model(*args).to(DEVICE)
 
     def reset(self):
         obs, _ = self.env.reset()
-        self.obs = torch.from_numpy(obs).float().detach()
+        self.obs = torch.from_numpy(obs).float().detach().to(DEVICE)
+        if self.pixels:
+            self.obs = transforms(self.env)
+
 
     def fill_memory(self, num):
         self.reset()
@@ -92,19 +125,22 @@ class Agent:
     def step(self, eps):
         action = self.get_action(eps)
         obs_n, reward, term, trunc, _ = self.env.step(action)
-        obs_n = torch.from_numpy(obs_n).float().detach()
+        obs_n = torch.from_numpy(obs_n).float().detach().to(DEVICE)
+        if self.pixels:
+            obs_n = transforms(self.env)
         done = term or trunc
         exp = (self.obs, action, reward, done, obs_n)
         self.memory.append(exp)
         self.obs = obs_n
         return reward, done
     
-    def fit(self, eps_fn, gamma, opt,  bs, timesteps):
+    def fit(self, eps_fn, gamma, opt,  bs, step, max_steps):
         eps_reward = 0
         eps_loss = 0
         steps = 0
-        for t in range(timesteps):
-            eps = eps_fn(t)
+        self.q.train()
+        for t in range(max_steps):
+            eps = eps_fn(step + t)
             reward, eps_done = self.step(eps)
             eps_reward += reward
 
@@ -113,7 +149,7 @@ class Agent:
             (obs, action, reward, done, obs_n) = self.memory.sample(bs)
             q_vals = self.q(obs_n).max(-1, keepdim=True).values
             tgt = reward + (1-done)*gamma*q_vals
-            pred = self.q(obs)[torch.arange(bs), action.reshape(-1)].reshape(-1,1)
+            pred = self.q(obs)[torch.arange(bs), action.reshape(-1)].reshape((-1,1))
             assert(tgt.shape == pred.shape), f'tgt:{tgt.shape} != pred:{pred.shape}'
             mse_loss = (tgt-pred).pow(2).mean()
             mse_loss.backward()
@@ -132,39 +168,53 @@ class Agent:
         self.reset()
         done = False
         steps = 0
+        reward = 0
         while not done and steps < max_steps:
             render = agent.env.render()
             replay.append(render)
             action = agent.get_action(-1)
-            self.obs, _, term, trunc, _ = agent.env.step(action)
-            self.obs = torch.from_numpy(self.obs)
+            self.obs, reward, term, trunc, _ = agent.env.step(action)
+            self.obs = torch.from_numpy(self.obs).float().to(DEVICE)
+            if self.pixels:
+                self.obs = transforms(self.env)
             done = term or trunc
             steps += 1
         render = agent.env.render()
         replay.append(render)
-        return replay
+        return replay, reward
 
 
 
 
 if __name__ == '__main__':
-    env_name = 'CartPole-v1'
+    # env_name = 'CartPole-v1'
+    env_name = 'LunarLander-v2'
     env = gym.make(env_name, render_mode='rgb_array')
     agent = Agent(env, DQNSimple, env.observation_space.shape[0], int(env.action_space.n), 128)
+    # agent = Agent(env, DQNPixel, int(env.action_space.n), 256)
     opt = optim.Adam(agent.q.parameters(), lr=1e-3)
+    # eps = lambda t: max(0.99**(t/64), 0.1)
     eps = lambda _: 0.1
     gamma = 0.99
-    bs = 32
+    bs = 64
 
-    agent.fill_memory(100)
+    agent.fill_memory(1000)
+    num_eps = 1500
     total_reward = 0
-    num_eps = 500
+    steps = 0
     for n in range(num_eps):
         agent.reset()
-        reward, loss, steps = agent.fit(eps, gamma, opt, bs, 10_000)
+        reward, loss, steps = agent.fit(eps, gamma, opt, bs, steps, 100_000)
         total_reward += reward
-        print(f'Episode {n} - reward: {reward} - avg reward: {total_reward/(n+1)} - loss: {loss}')
-    replay = agent.play(10_000)
-    save_replay('.', 'dqn-cartpole', replay, 30)
-    print(f'saved replay: {len(replay)} frames')
+        if n % 50 == 0:
+            print(f'Episode {n} - reward: {reward} - avg reward: {total_reward/50} - loss: {loss}')
+            total_reward = 0
+    
+    # generate about 10 replays
+    for i in range(10):
+        replay = agent.play(100_000)
+        save_replay('.', f'dqn-{env_name[:-3].lower()}-{i}', replay, 30)
+        print(f'saved replay: {len(replay)} frames')
+        print(f'reward: {reward}')
+        print()
     env.close()
